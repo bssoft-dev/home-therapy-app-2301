6cc8d450c86dc3137c8fe6b1f21d186487299dca
bs-jes <esjeoung@bs-soft.co.kr>
2023-08-24 09:26:07 +0900

    불필요한 데이터 제거

--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:home_therapy_app/widgets/custom_button_widget.dart';
-
 basicAppBar(BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) {
   return PreferredSize(
       preferredSize: const Size.fromHeight(60.0),
       child: AppBar(
         centerTitle: true,
-        leading: simpleIconButton(
-          Icons.menu,
-          30,
-          () => scaffoldKey.currentState?.openDrawer(),
-        ),
+        // leading: simpleIconButton(
+        //   Icons.menu,
+        //   30,
+        //   () => scaffoldKey.currentState?.openDrawer(),
+        // ),
         // actions: [
         //   simpleIconButton(
         //     Icons.settings,
